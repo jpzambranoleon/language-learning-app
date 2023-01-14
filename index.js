69c2b46aebeb7447966de5a5601233d360cbf94c
Jean-Paul <jpzambrano.leon@gmail.com>
2023-01-13 21:47:02 -0600

    updated index.js for openai to work with client

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
+// 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
+
 const { Configuration, OpenAIApi } = require("openai");
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 const configuration = new Configuration({
   organization: "org-UmsTiOJ79hIOscV8kXQneBET",
-  apiKey: "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",
+  apiKey: "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",
 });
+
 const openai = new OpenAIApi(configuration);
 
-// create a simple express api that calls the function above
-
 const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
 const port = 3080;
 
 app.post("/", async (req, res) => {
+  const { message } = req.body;
   const response = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: "Say this is a test",
-    max_tokens: 7,
-    temperature: 0,
+    prompt: `${message}`,
+    max_tokens: 100,
+    temperature: 0.5,
   });
-  console.log(response.data.choices[0].text);
   res.json({
-    data: response.data,
+    message: response.data.choices[0].text,
   });
 });
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
