8296c4f5e3ab8ffde55cde8edbf859f72310ccf8
Jean-Paul <jpzambrano.leon@gmail.com>
2023-01-13 21:47:32 -0600

    updated Message.jsx

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -10,7 +10,7 @@ const Message = ({ message }) => {
             <Grid
               item
               sx={{
-                maxWidth: 350,
+                maxWidth: { lg: 360, xs: 200 },
                 backgroundColor: green[100],
                 borderRadius: 3,
               }}
@@ -22,18 +22,11 @@ const Message = ({ message }) => {
               src="https://material-ui.com/static/images/avatar/1.jpg"
               sx={{ ml: 1, width: 25, height: 25 }}
             />
-            <Grid item xs={12}>
-              <ListItemText
-                align="right"
-                secondary="message created at"
-                sx={{ mr: 4 }}
-              ></ListItemText>
-            </Grid>
           </Grid>
         </ListItem>
       ) : (
         <ListItem>
-          <Grid container justifyContent="flex-start" alignItems="center">
+          <Grid container justifyContent="flex-start">
             <Avatar
               alt="Remy Sharp"
               src="https://material-ui.com/static/images/avatar/3.jpg"
@@ -42,19 +35,12 @@ const Message = ({ message }) => {
             <Grid
               item
               sx={{
-                maxWidth: 350,
+                maxWidth: { lg: 360, xs: 215 },
                 backgroundColor: blue[100],
                 borderRadius: 3,
               }}
             >
               <ListItemText sx={{ pl: 2, pr: 2 }} primary={message.message} />
-            </Grid>
-            <Grid item xs={12}>
-              <ListItemText
-                align="left"
-                secondary="message created at"
-                sx={{ ml: 4 }}
-              ></ListItemText>
             </Grid>
           </Grid>
         </ListItem>
