a246b501bdd4b6eee8dd65af351880ec55a07f7f
Jean-Paul <jpzambrano.leon@gmail.com>
2023-01-13 21:47:55 -0600

    updatd Chatbot.jsx

--- client/src/pages/Chatbot.jsx
+++ client/src/pages/Chatbot.jsx
@@ -1,239 +1,168 @@
 import { Send } from "@mui/icons-material";
 import {
-  Avatar,
   Box,
   Container,
   Divider,
-  Grid,
   IconButton,
   InputBase,
   List,
-  ListItem,
-  ListItemText,
   Paper,
   Toolbar,
 } from "@mui/material";
-import { blue, green } from "@mui/material/colors";
-import { useState } from "react";
+import { useEffect, useState, useRef } from "react";
+import Message from "../components/Message";
+import MuiDrawer from "../components/MuiDrawer";
 
 export default function Chatbot() {
   const [input, setInput] = useState("");
+  const scrollRef = useRef();
   const [chatLog, setChatLog] = useState([]);
+
+  // clear chats
+  function clearChat() {
+    setChatLog([]);
+  }
 
   async function handleSubmit(e) {
     e.preventDefault();
-    setChatLog([...chatLog, { user: "me", message: `${input}` }]);
+    let chatLogNew = [...chatLog, { user: "me", message: `${input}` }];
     setInput("");
+    setChatLog(chatLogNew);
+
+    const messages = chatLogNew.map((message) => message.message).join("\n");
+
+    const response = await fetch("http://localhost:3080/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: messages,
+      }),
+    });
+
+    const data = await response.json();
+    setChatLog([...chatLogNew, { user: "gpt", message: `${data.message}` }]);
   }
 
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [chatLog]);
+
   return (
-    <Box
-      component="main"
-      sx={{
-        backgroundColor: (theme) =>
-          theme.palette.mode === "light"
-            ? theme.palette.grey[100]
-            : theme.palette.grey[900],
-        flexGrow: 1,
-        height: "100vh",
-        position: "relative",
-      }}
-    >
-      <Toolbar sx={{ display: { lg: "block", xs: "none" } }} />
-      <Container maxWidth="md" sx={{ height: "84vh", overflowY: "auto" }}>
-        <List>
-          <ListItem>
-            <Grid container justifyContent="flex-end">
-              <Grid
-                item
-                xs={12}
-                sm={6}
-                sx={{ backgroundColor: green[100], borderRadius: 3 }}
-              >
-                <ListItemText
-                  sx={{ pl: 2, pr: 2 }}
-                  primary="Hello chatgpt. My name is Jean-Paul Zambrano-Leon and I need assistance. Will you help me? I know that you are way smarter than me. I can speak multiple languages just like you."
-                />
-              </Grid>
-              <Avatar
-                alt="Remy Sharp"
-                src="https://material-ui.com/static/images/avatar/1.jpg"
-                sx={{ ml: 1, width: 25, height: 25 }}
-              />
-              <Grid item xs={12}>
-                <ListItemText
-                  align="right"
-                  secondary="message created at"
-                  sx={{ mr: 4 }}
-                ></ListItemText>
-              </Grid>
-            </Grid>
-          </ListItem>
-          <ListItem>
-            <Grid container justifyContent="flex-start">
-              <Avatar
-                alt="Remy Sharp"
-                src="https://material-ui.com/static/images/avatar/1.jpg"
-                sx={{ mr: 1, width: 25, height: 25 }}
-              />
-              <Grid
-                item
-                xs={12}
-                sm={6}
-                sx={{ backgroundColor: blue[100], borderRadius: 3 }}
-              >
-                <ListItemText
-                  sx={{ pl: 2, pr: 2 }}
-                  primary="Hello Jean-Paul. I am your artificial inteligence assistant chatgpt."
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <ListItemText
-                  align="left"
-                  secondary="message created at"
-                  sx={{ ml: 4 }}
-                ></ListItemText>
-              </Grid>
-            </Grid>
-          </ListItem>
-          <ListItem>
-            <Grid container justifyContent="flex-start">
-              <Grid
-                item
-                xs={12}
-                sm={6}
-                sx={{ backgroundColor: blue[100], borderRadius: 3 }}
-              >
-                <ListItemText
-                  sx={{ pl: 2, pr: 2 }}
-                  primary="I am programmed to be your personal assisant. I use the latest natural language processing technology in the market. I can be used to solve complicated problems that can help you learn."
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <ListItemText
-                  align="left"
-                  secondary="message created at"
-                ></ListItemText>
-              </Grid>
-            </Grid>
-          </ListItem>
-          <ListItem>
-            <Grid container justifyContent="flex-end">
-              <Grid
-                item
-                xs={12}
-                sm={6}
-                sx={{ backgroundColor: green[100], borderRadius: 3 }}
-              >
-                <ListItemText
-                  sx={{ pl: 2, pr: 2 }}
-                  primary="Wow thank you so much chatgpt! I'm definatly going to use you as my assistant and we can solve many problems together. Do you like have a personality too or are you just a robot? I'm scared of robots you know. I'm not going to like you if you are a robot."
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <ListItemText
-                  align="right"
-                  secondary="message created at"
-                ></ListItemText>
-              </Grid>
-            </Grid>
-          </ListItem>
-          <ListItem>
-            <Grid container justifyContent="flex-end">
-              <Grid
-                item
-                xs={12}
-                sm={6}
-                sx={{ backgroundColor: green[100], borderRadius: 3 }}
-              >
-                <ListItemText
-                  sx={{ pl: 2, pr: 2 }}
-                  primary="I will do my best to impress you and I will work hard to become successful. So help me God! I will become the next Steve Jobs."
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <ListItemText
-                  align="right"
-                  secondary="message created at"
-                ></ListItemText>
-              </Grid>
-            </Grid>
-          </ListItem>
-          <ListItem>
-            <Grid container justifyContent="flex-end">
-              <Grid
-                item
-                xs={12}
-                sm={6}
-                sx={{ backgroundColor: green[100], borderRadius: 3 }}
-              >
-                <ListItemText
-                  sx={{ pl: 2, pr: 2 }}
-                  primary="I will do my best to impress you and I will work hard to become successful. So help me God! I will become the next Steve Jobs."
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <ListItemText
-                  align="right"
-                  secondary="message created at"
-                ></ListItemText>
-              </Grid>
-            </Grid>
-          </ListItem>
-          <ListItem>
-            <Grid container justifyContent="flex-end">
-              <Grid
-                item
-                xs={12}
-                sm={6}
-                sx={{ backgroundColor: green[100], borderRadius: 3 }}
-              >
-                <ListItemText
-                  sx={{ pl: 2, pr: 2 }}
-                  primary="I will do my best to impress you and I will work hard to become successful. So help me God! I will become the next Steve Jobs."
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <ListItemText
-                  align="right"
-                  secondary="message created at"
-                ></ListItemText>
-              </Grid>
-            </Grid>
-          </ListItem>
-        </List>
-      </Container>
-      <Container
-        maxWidth="md"
-        sx={{ position: "absolute", bottom: 0, left: 0, right: 0 }}
+    <>
+      <MuiDrawer clearChat={clearChat} />
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: (theme) =>
+            theme.palette.mode === "light"
+              ? theme.palette.grey[100]
+              : theme.palette.grey[900],
+          flexGrow: 1,
+          height: "100vh",
+          position: "relative",
+        }}
       >
-        <Paper
-          component="form"
+        <Toolbar />
+        <Container
+          maxWidth="md"
           sx={{
-            margin: "12px",
-            p: "2px 4px",
-            display: "flex",
-            alignItems: "center",
+            height: { lg: "84vh", xs: "84vh" },
+            overflowY: "auto",
+            display: { xl: "block", xs: "none" },
           }}
         >
-          <InputBase
-            sx={{ ml: 1, flex: 1 }}
-            placeholder="What's on your mind?"
-            inputProps={{ "aria-label": "what's on your mind" }}
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-          />
-          <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-          <IconButton
-            color="primary"
-            sx={{ p: "10px" }}
-            aria-label="directions"
-            onClick={handleSubmit}
+          <List>
+            {chatLog.map((message, index) => (
+              <div ref={scrollRef}>
+                <Message message={message} key={index} />
+              </div>
+            ))}
+          </List>
+        </Container>
+        <Box
+          sx={{
+            height: { xs: "84vh" },
+            overflow: "auto",
+            display: { xl: "none", xs: "block" },
+          }}
+        >
+          <List>
+            {chatLog.map((message, index) => (
+              <div ref={scrollRef}>
+                <Message message={message} key={index} />
+              </div>
+            ))}
+          </List>
+        </Box>
+        <Container
+          maxWidth="md"
+          sx={{
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            right: 0,
+          }}
+        >
+          <Paper
+            component="form"
+            onSubmit={handleSubmit}
+            sx={{
+              margin: "12px",
+              p: "2px 4px",
+              display: { lg: "flex", xs: "none" },
+              alignItems: "center",
+            }}
           >
-            <Send />
-          </IconButton>
-        </Paper>
-      </Container>
-    </Box>
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="What's on your mind?"
+              inputProps={{ "aria-label": "what's on your mind" }}
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            <IconButton
+              color="primary"
+              sx={{ p: "10px" }}
+              aria-label="directions"
+              onClick={handleSubmit}
+            >
+              <Send />
+            </IconButton>
+          </Paper>
+        </Container>
+        <Box sx={{ position: "absolute", bottom: 0, left: 0, right: 0 }}>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            sx={{
+              bgcolor: "background.paper",
+              display: { lg: "none", xs: "flex" },
+              p: "2px 4px",
+              alignItems: "center",
+            }}
+          >
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="What's on your mind?"
+              inputProps={{ "aria-label": "what's on your mind" }}
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
+            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            <IconButton
+              color="primary"
+              sx={{ p: "10px" }}
+              aria-label="directions"
+              onClick={handleSubmit}
+            >
+              <Send />
+            </IconButton>
+          </Box>
+        </Box>
+      </Box>
+    </>
   );
 }
