d025468d7ccc6ebba52b39aa9481237e99e8c99c
Jean-Paul <jpzambrano.leon@gmail.com>
2023-01-13 21:47:19 -0600

    moved components into child components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,5 @@
 import { Box, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import MuiDrawer from "./components/MuiDrawer";
-import Dashboard from "./Dashboard";
-import Chat from "./pages/Chat";
 import Chatbot from "./pages/Chatbot";
 
 const mdTheme = createTheme();
@@ -13,12 +10,9 @@ function App() {
       <ThemeProvider theme={mdTheme}>
         <Box sx={{ display: "flex" }}>
           <CssBaseline />
-          <MuiDrawer />
           <Router>
             <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/chat" element={<Chat />} />
-              <Route path="/chatbot" element={<Chatbot />} />
+              <Route path="/" element={<Chatbot />} />
             </Routes>
           </Router>
         </Box>
