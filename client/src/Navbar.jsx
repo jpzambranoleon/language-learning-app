fe2839b0054e00b7cf577db12e2e184f33194d23
Jean-Paul <jpzambrano.leon@gmail.com>
2023-01-15 00:20:52 -0600

    updated Navbar.jsx

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -7,11 +7,14 @@ import {
   IconButton,
   InputBase,
   Link,
+  List,
   Menu,
   Toolbar,
   Typography,
 } from "@mui/material";
 import { grey } from "@mui/material/colors";
+import { useEffect, useRef, useState } from "react";
+import Message from "./components/Message";
 
 function Copyright(props) {
   return (
@@ -20,6 +23,7 @@ function Copyright(props) {
       color="text.secondary"
       align="center"
       {...props}
+      sx={{ mt: 2 }}
     >
       {"Copyright © "}
       <Link color="inherit" href="https://leonedigitale.com/">
@@ -32,28 +36,94 @@ function Copyright(props) {
 }
 
 export default function Navbar() {
+  const [input, setInput] = useState("");
+  const scrollRef = useRef();
+  const [chatLog, setChatLog] = useState([]);
+
+  // clear chats
+  function clearChat() {
+    setChatLog([]);
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    let chatLogNew = [...chatLog, { user: "me", message: `${input}` }];
+    setInput("");
+    setChatLog(chatLogNew);
+
+    const messages = chatLogNew.map((message) => message.message).join("\n");
+
+    const response = await fetch("/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: messages,
+      }),
+    });
+
+    const data = await response.json();
+    setChatLog([...chatLogNew, { user: "gpt", message: `${data.message}` }]);
+  }
+
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [chatLog]);
+
   return (
-    <Box sx={{ position: "absolute", bottom: 0, left: 0, right: 0 }}>
+    <Box
+      component="main"
+      sx={{
+        backgroundColor: (theme) =>
+          theme.palette.mode === "light"
+            ? theme.palette.grey[100]
+            : theme.palette.grey[900],
+        flexGrow: 1,
+        height: "100vh",
+      }}
+    >
       <Box
-        component="form"
         sx={{
-          bgcolor: "background.paper",
-          display: "flex",
-          p: "2px 4px",
-          alignItems: "center",
+          height: { xs: "85vh" },
+          overflow: "auto",
+          display: { xl: "none", xs: "block" },
         }}
       >
-        <InputBase
-          sx={{ ml: 1, flex: 1 }}
-          placeholder="What's on your mind?"
-          inputProps={{ "aria-label": "what's on your mind" }}
-        />
-        <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
-        <IconButton color="primary" sx={{ p: "10px" }} aria-label="directions">
-          <Send />
-        </IconButton>
+        <List>
+          {chatLog.map((message, index) => (
+            <div ref={scrollRef}>
+              <Message message={message} key={index} />
+            </div>
+          ))}
+        </List>
       </Box>
-      <Copyright />
+      <Box>
+        <Box
+          component="form"
+          sx={{
+            bgcolor: "background.paper",
+            display: "flex",
+            p: "2px 4px",
+            alignItems: "center",
+          }}
+        >
+          <InputBase
+            sx={{ ml: 1, flex: 1 }}
+            placeholder="What's on your mind?"
+            inputProps={{ "aria-label": "what's on your mind" }}
+          />
+          <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+          <IconButton
+            color="primary"
+            sx={{ p: "10px" }}
+            aria-label="directions"
+          >
+            <Send />
+          </IconButton>
+        </Box>
+        <Copyright />
+      </Box>
     </Box>
   );
 }
