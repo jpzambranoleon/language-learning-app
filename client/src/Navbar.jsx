05908ad7a2ec15ef4f92a1d67b535ebc5c0ad0ab
Jean-Paul <jpzambrano.leon@gmail.com>
2023-01-14 18:28:12 -0600

    created Navbar.jsx

---
+++ client/src/Navbar.jsx
@@ -0,0 +1,92 @@
+import { Send } from "@mui/icons-material";
+import {
+  AppBar,
+  Box,
+  Button,
+  Divider,
+  IconButton,
+  InputBase,
+  Link,
+  Menu,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { grey } from "@mui/material/colors";
+
+function Copyright(props) {
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      {...props}
+    >
+      {"Copyright © "}
+      <Link color="inherit" href="https://leonedigitale.com/">
+        Leone Digitale
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+export default function Navbar() {
+  return (
+    <>
+      <AppBar>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <Menu />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: grey[100],
+          flexGrow: 1,
+          height: "100vh",
+          position: "relative",
+        }}
+      >
+        <Box sx={{ position: "absolute", bottom: 0, left: 0, right: 0 }}>
+          <Box
+            component="form"
+            sx={{
+              bgcolor: "background.paper",
+              display: "flex",
+              p: "2px 4px",
+              alignItems: "center",
+            }}
+          >
+            <InputBase
+              sx={{ ml: 1, flex: 1 }}
+              placeholder="What's on your mind?"
+              inputProps={{ "aria-label": "what's on your mind" }}
+            />
+            <Divider sx={{ height: 28, m: 0.5 }} orientation="vertical" />
+            <IconButton
+              color="primary"
+              sx={{ p: "10px" }}
+              aria-label="directions"
+            >
+              <Send />
+            </IconButton>
+          </Box>
+          <Copyright />
+        </Box>
+      </Box>
+    </>
+  );
+}
