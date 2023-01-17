2c1f717a49557e9b46bd7dbc10d87fa096edd40d
Jean-Paul <jpzambrano.leon@gmail.com>
2023-01-17 14:02:54 -0600

    created user.model.js

---
+++ models/user.model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    firstName: { type: String, max: 20 },
+    lastName: { type: String, max: 20 },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    active: { type: Boolean, default: false, required: true },
+  },
+  {
+    timestamps: {
+      createdAt: "createdAt",
+      updatedAt: "updatedAt",
+    },
+  }
+);
+
+const User = mongoose.model("user", userSchema);
+module.exports = User;
+
+module.exports.hashPassword = async (password) => {
+  try {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+  } catch (err) {
+    throw new Error("hashing failed", err);
+  }
+};
