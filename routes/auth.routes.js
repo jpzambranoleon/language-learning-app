190185e279faa5c8203daffff79e04f2beb0fe0e
Jean-Paul <jpzambrano.leon@gmail.com>
2023-01-18 23:57:33 -0600

    modified file

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,6 +1,14 @@
 const router = require("express").Router();
-const authController = require("../controllers/user.controller");
+const authController = require("../controllers/auth.controller");
+const {
+  checkDuplicateEmail,
+  verrifyEmail,
+} = require("../middlewares/register.middleware");
 
-router.post("/register", authController.register);
+router.post(
+  "/register",
+  [checkDuplicateEmail, verrifyEmail],
+  authController.register
+);
 
 module.exports = router;
