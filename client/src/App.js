d7e07fb32d71a0242424e27cb6d613485279752d
Jean-Paul <jpzambrano.leon@gmail.com>
2023-01-20 14:50:40 -0600

    modified App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,10 @@ import { Box, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import axios from "axios";
 import { useSelector } from "react-redux";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import MuiDrawer from "./components/MuiDrawer";
 import Chatbot from "./pages/Chatbot";
 import Login from "./pages/Login";
+import Messages from "./pages/Messages";
 import Register from "./pages/Register";
 import SendEmail from "./pages/SendEmail";
 import VerifyEmail from "./pages/VerifyEmail";
@@ -23,25 +25,18 @@ function App() {
         >
           <CssBaseline />
           <Router>
-            {currentUser ? (
-              <Routes>
-                <Route path="/" element={<Chatbot />} />
-              </Routes>
-            ) : (
-              <Routes>
-                <Route path="/" element={<Login />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/register" element={<Register />} />
-                <Route
-                  path="/send/verification/:email"
-                  element={<SendEmail />}
-                />
-                <Route
-                  path="/verify/email/:username/:token"
-                  element={<VerifyEmail />}
-                />
-              </Routes>
-            )}
+            <MuiDrawer />
+            <Routes>
+              <Route path="/" element={<Chatbot />} />
+              <Route path="/messages" element={<Messages />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/send/verification/:email" element={<SendEmail />} />
+              <Route
+                path="/verify/email/:username/:token"
+                element={<VerifyEmail />}
+              />
+            </Routes>
           </Router>
         </Box>
       </ThemeProvider>
