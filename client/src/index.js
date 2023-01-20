0fa94e5b1e3f97c66dc3a629a5cbe63862fcc998
Jean-Paul <jpzambrano.leon@gmail.com>
2023-01-19 23:33:49 -0600

    added context api and redux providers

--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { store } from "./redux/store";
+import { Provider } from "react-redux";
+import { InfoProvider } from "./utils/InfoProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <InfoProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </InfoProvider>
   </React.StrictMode>
 );
