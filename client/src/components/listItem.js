b5ae098acde89cf6db5f1670ca69445d1deffe1a
Jean-Paul <jpzambrano.leon@gmail.com>
2023-01-23 18:53:52 -0600

    modified file

--- client/src/components/listItem.js
+++ client/src/components/listItem.js
@@ -1,9 +1,13 @@
 import {
   Assignment,
   BarChart,
+  Chat,
   Dashboard,
+  Home,
   Layers,
+  Logout,
   People,
+  Person,
   ShoppingCart,
 } from "@mui/icons-material";
 import {
@@ -13,38 +17,27 @@ import {
   ListSubheader,
 } from "@mui/material";
 import * as React from "react";
+import { Link } from "react-router-dom";
 
 export const mainListItems = (
   <React.Fragment>
-    <ListItemButton>
+    <ListItemButton component={Link} to="/">
       <ListItemIcon>
-        <Dashboard />
+        <Home />
       </ListItemIcon>
-      <ListItemText primary="Dashboard" />
+      <ListItemText primary="Home" />
     </ListItemButton>
-    <ListItemButton>
+    <ListItemButton component={Link} to="/messages">
       <ListItemIcon>
-        <ShoppingCart />
+        <Chat />
       </ListItemIcon>
-      <ListItemText primary="Orders" />
+      <ListItemText primary="Chats" />
     </ListItemButton>
-    <ListItemButton>
+    <ListItemButton component={Link} to="/profile">
       <ListItemIcon>
-        <People />
+        <Person />
       </ListItemIcon>
-      <ListItemText primary="Customers" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <BarChart />
-      </ListItemIcon>
-      <ListItemText primary="Reports" />
-    </ListItemButton>
-    <ListItemButton>
-      <ListItemIcon>
-        <Layers />
-      </ListItemIcon>
-      <ListItemText primary="Integrations" />
+      <ListItemText primary="Profile" />
     </ListItemButton>
   </React.Fragment>
 );
