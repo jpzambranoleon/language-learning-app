455805061c85328714f9a67d1f7d58e7121af45d
Jean-Paul <jpzambrano.leon@gmail.com>
2023-01-28 12:59:18 -0600

    modified file

--- index.js
+++ index.js
@@ -46,6 +46,8 @@ app.post("/", async (req, res) => {
 // serving the frontend
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/users", require("./routes/users.routes"));
+app.use("/api/conversations", require("./routes/conversation.routes"));
+app.use("/api/messages", require("./routes/message.routes"));
 
 // error handler
 app.use((err, req, res, next) => {
