a9f2dde8d6609318b8ec20aa27fafc6fb8f5e9c3
Jean-Paul <jpzambrano.leon@gmail.com>
2023-02-14 17:13:08 -0600

    modified index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const { Configuration, OpenAIApi } = require("openai");
 const express = require("express");
 const cors = require("cors");
 const MongooseConnection = require("./utility/mongoose.connection");
@@ -6,6 +5,7 @@ const path = require("path");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 // Create application with express
@@ -17,37 +17,19 @@ MongooseConnection();
 // Middlewares
 app.use(cookieParser());
 app.use(express.json());
+app.use(bodyParser.json());
 app.use(cors());
 app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 app.use(morgan("common"));
 
-const configuration = new Configuration({
-  organization: "org-UmsTiOJ79hIOscV8kXQneBET",
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-const openai = new OpenAIApi(configuration);
-
 const PORT = process.env.PORT || 8000;
-
-app.post("/", async (req, res) => {
-  const { message } = req.body;
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: `${message}`,
-    max_tokens: 100,
-    temperature: 0.5,
-  });
-  res.json({
-    message: response.data.choices[0].text,
-  });
-});
 
 // serving the frontend
 app.use("/api/auth", require("./routes/auth.routes"));
 app.use("/api/users", require("./routes/users.routes"));
 app.use("/api/conversations", require("./routes/conversation.routes"));
 app.use("/api/messages", require("./routes/message.routes"));
+app.use("/api/assistants", require("./routes/assistant.routes"));
 
 // error handler
 app.use((err, req, res, next) => {
