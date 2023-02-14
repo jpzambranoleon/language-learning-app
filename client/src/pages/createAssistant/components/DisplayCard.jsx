cb84ebc7b4b877ce8db293604836469fc28a6d26
Jean-Paul <jpzambrano.leon@gmail.com>
2023-02-14 17:39:15 -0600

    modified file

--- client/src/pages/createAssistant/components/DisplayCard.jsx
+++ client/src/pages/createAssistant/components/DisplayCard.jsx
@@ -7,12 +7,12 @@ import {
   CardHeader,
   Chip,
   Collapse,
+  Grid,
   IconButton,
   Stack,
   styled,
   Typography,
 } from "@mui/material";
-import { red } from "@mui/material/colors";
 import { useState } from "react";
 
 const ExpandMoreStyle = styled((props) => {
@@ -34,66 +34,86 @@ const DisplayCard = ({ name, model, prompt, file }) => {
   };
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardHeader
-        avatar={
-          <Avatar src={!file ? "/broken-img.jpg" : URL.createObjectURL(file)} />
-        }
-        action={
-          <IconButton aria-label="settings">
-            <MoreVert />
-          </IconButton>
-        }
-        title={name}
-        subheader="September 14, 2016"
-      />
-      <CardContent>
-        <Typography paragraph variant="body1">
-          Name: {name}
+    <Grid container>
+      <Grid item xs={6}>
+        <Typography variant="body2" paragraph>
+          Your assistant will be displayed in a card like the one on the right.
+          Be sure to review your assisant before continuing.
         </Typography>
-        <Typography paragraph variant="body1">
-          Model: {model.model}
+        <Typography variant="body2" paragraph>
+          Each assisant you create will have it's own card. They will show up on
+          the assisants tab of your profile.
         </Typography>
-        <Typography paragraph variant="body1">
-          Strengths:
+        <Typography variant="body2" paragraph>
+          If you're happy with your settings, you can finish creating your
+          personal assisant by clicking on <b>Finish</b>.
         </Typography>
-        <Stack direction="row" sx={{ flexWrap: "wrap", gap: 1 }}>
-          {model.strengths.map((strengths, index) => {
-            return (
-              <Chip
-                key={index}
-                label={strengths}
-                size="small"
-                color="primary"
+      </Grid>
+      <Grid item xs={6} display="flex" justifyContent="flex-end">
+        <Card sx={{ maxWidth: 345 }}>
+          <CardHeader
+            avatar={
+              <Avatar
+                src={!file ? "/broken-img.jpg" : URL.createObjectURL(file)}
               />
-            );
-          })}
-        </Stack>
-      </CardContent>
-      <CardActions disableSpacing>
-        <ExpandMoreStyle
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMore />
-        </ExpandMoreStyle>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography paragraph>Prompt:</Typography>
-          <Typography
-            paragraph
-            variant="body2"
-            sx={{ whiteSpace: "pre-line" }}
-            color="text.secondary"
-          >
-            {prompt}
-          </Typography>
-        </CardContent>
-      </Collapse>
-    </Card>
+            }
+            action={
+              <IconButton aria-label="settings">
+                <MoreVert />
+              </IconButton>
+            }
+            title={name}
+            subheader="September 14, 2016"
+          />
+          <CardContent>
+            <Typography paragraph variant="body1">
+              Name: {name}
+            </Typography>
+            <Typography paragraph variant="body1">
+              Model: {model.model}
+            </Typography>
+            <Typography paragraph variant="body1">
+              Strengths:
+            </Typography>
+            <Stack direction="row" sx={{ flexWrap: "wrap", gap: 1 }}>
+              {model.strengths.map((strengths, index) => {
+                return (
+                  <Chip
+                    key={index}
+                    label={strengths}
+                    size="small"
+                    color="primary"
+                  />
+                );
+              })}
+            </Stack>
+          </CardContent>
+          <CardActions disableSpacing>
+            <ExpandMoreStyle
+              expand={expanded}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMore />
+            </ExpandMoreStyle>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography paragraph>Prompt:</Typography>
+              <Typography
+                paragraph
+                variant="body2"
+                sx={{ whiteSpace: "pre-line" }}
+                color="text.secondary"
+              >
+                {prompt}
+              </Typography>
+            </CardContent>
+          </Collapse>
+        </Card>
+      </Grid>
+    </Grid>
   );
 };
 
