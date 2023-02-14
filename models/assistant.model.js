2e777a1a5243607f396c69a4e776602e32b0d284
Jean-Paul <jpzambrano.leon@gmail.com>
2023-02-14 17:12:14 -0600

    modified assistant.model.js

--- models/assistant.model.js
+++ models/assistant.model.js
@@ -3,8 +3,31 @@ const Schema = mongoose.Schema;
 
 const assistantSchema = new Schema(
   {
+    userId: { type: String },
     name: { type: String, max: 50 },
-    profilePic: { type: String, default: "" },
+    model: { type: String, max: 50 },
+    strengths: [],
+    avatar: { type: String, default: "" },
+    prompt: { type: String },
+    chatLog: [],
+    temperature: { type: Number },
+    maxToken: {
+      type: Number,
+      required: true,
+      integer: true,
+      get: (v) => Math.round(v),
+      set: (v) => Math.round(v),
+    },
+    topP: { type: Number },
+    frequencyPenalty: { type: Number },
+    presencePenalty: { type: Number },
+    bestOf: {
+      type: Number,
+      required: true,
+      integer: true,
+      get: (v) => Math.round(v),
+      set: (v) => Math.round(v),
+    },
   },
   {
     timestamps: {
