ccc71353fbcaf45aebae53001ab359919c380421
Jean-Paul Zambrano-Leon <63323302+jpzambranoleon@users.noreply.github.com>
2023-07-11 21:37:37 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ require("dotenv").config();
 // Create application with express
 const app = express();
 
-// Reminder: fix issue urgent, like now please hurry like now please nigga i beg thee nigga nigga niggA don't you
+// Reminder: fix issue urgent, like now please hurry like now please nigga i beg thee nigga nigga niggA don't you run from
 
 // Connect to database
 MongooseConnection();
